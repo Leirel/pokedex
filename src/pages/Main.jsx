8687964f15c4c5f51886a0a1ea0954a89fe1c144
HikiLei <y2508189@gmail.com>
2025-08-29 12:22:22 +0900

    api 연결

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPokemonList } from "../RTK/pokemonSlice";
 import { Link } from "react-router-dom";
 
 const Main = () => {
-    const pokemons = [
-        { id: 1, name: "Bulbasaur", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" },
-        { id: 2, name: "Ivysaur", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" },
-    ];
+    const dispatch = useDispatch();
+    const { list, loading, error } = useSelector((state) => state.pokemon);
+
+    useEffect(() => {
+        dispatch(fetchPokemonList());
+    }, [dispatch]);
+
+    if (loading) return <p>⏳ 로딩 중...</p>;
+    if (error) return <p>❌ 에러 발생: {error}</p>;
 
     return (
         <section className="section-wrapper grid grid-cols-2 md:grid-cols-4 gap-4">
-            {pokemons.map((p) => (
+            {list.map((p) => (
                 <Link key={p.id} to={`/detail/${p.id}`} className="pokemon-card">
                     <img src={p.image} alt={p.name} />
                     <h2>{p.name}</h2>
                 </Link>
             ))}
         </section>
-
     );
 };
 
